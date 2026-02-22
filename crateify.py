20ba3f100ef74f31cf57c20e06aaf4978b7f64f1
Daniel King <damaki.gh@gmail.com>
2026-02-22 18:08:27 +0000

    Update GPR file instructions to also 'with' ravenscar_build.gpr
    The current instructions tell the user to "with" runtime_build.gpr only in
    their project file. For light-tasking/embedded runtimes Alire normally
    auto-withs ravenscar_build.gpr in the auto-generated config file. But if
    the user doesn't "with" that config in their main project file, or if
    Alire is configured with auto-with-gpr=false then ravenscar_build.gpr
    will not get built, leading to errors when building complaining that
    ali files for libgnarl units don't exist.

--- crateify.py
+++ crateify.py
@@ -15,6 +15,15 @@ import argparse
 import pathlib
 from typing import Dict
 
+project_file_withs_light = """\
+   with "runtime_build.gpr";\
+"""
+
+project_file_withs_ravenscar = """\
+   with "runtime_build.gpr";
+   with "ravenscar_build.gpr";\
+"""
+
 
 def patch_ravenscar_build_gpr(gpr_file: pathlib.Path, profile: str, target: str):
     """Patch the ravenscar_build.gpr file to add some style switches"""
@@ -143,7 +152,10 @@ def main():
         "pretty_target": pretty_target,
         "project_files_list": str(project_files),
         "version": args.version,
-        "languages_list": ", ".join(f'"{lang}"' for lang in languages_list)
+        "languages_list": ", ".join(f'"{lang}"' for lang in languages_list),
+        "project_file_withs": (
+            project_file_withs_ravenscar if has_libgnarl else project_file_withs_light
+        ),
     }
 
     templates_dir = pathlib.Path(__file__).parent / "templates"
